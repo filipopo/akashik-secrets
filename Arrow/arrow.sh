55c607e3d7f49af055cc3b70d09fbe42ffa86a59
tarhunthethundergod <127444721+tarhunthethundergod@users.noreply.github.com>
2024-02-09 17:11:12 +0100

    Refactored Arrow.sh code.

--- Arrow/arrow.sh
+++ Arrow/arrow.sh
@@ -2,10 +2,15 @@
 
 if (($1 < 3)); then
   echo "A bigger arrow is required to slay the beast."
-  else
-    echo "  Δ"
-    for ((i=0; i<=$1-3; i++)); do
-      echo "  |"
-    done
-    echo "  ⋂"
+  exit
 fi
+
+# Arrow point
+echo "  Δ"
+
+for ((i=0; i <= $1-3; i++)); do
+  echo "  |"
+done
+
+# Arrow hen
+echo "  ⋂"
