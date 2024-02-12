18e50560559bcd5e3f667b06c543f4bb991a9072
Filip <filipmymail@gmail.com>
2024-02-12 13:29:06 +0100

    Right adjusted string

--- Pyramid/pyramid-christmas-tree.py
+++ Pyramid/pyramid-christmas-tree.py
@@ -4,8 +4,7 @@ n = int(input('How many layers?: '))
 
 def pyramid(n, padding = 0):
   for i in range(1, n + 1):
-    print(" " * (n - i) + " " * padding, end="")
-    print("^ " * i)
+    print(('^ ' * i).rjust(padding + n + i))
 
 for i in range(1, n + 1):
   pyramid(i, n - i)
